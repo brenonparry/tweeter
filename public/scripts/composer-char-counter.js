038bb7927ae92a0c0b5b103e3416c11b0c2dd06d
Brenon <brenonparry@gmail.com>
2021-12-17 05:33:54 +0000

    Clear commented out code and add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,5 @@
+// FUNCTION FOR KEEPING TRACK OF THE NUMBER OF CHARACTERS //
+
 $(document).ready(function() {
   $("textarea").on("input", function() {
     
@@ -5,9 +7,9 @@ $(document).ready(function() {
     $('output').val(140 - tweet)
     
     if ($('output').val() < 0) {
-      $('output').addClass("negative-counter")
+      $('output').addClass("negative-counter") // Add a class to be styled if counter goes below 0
     } else {
-      $('output').removeClass("negative-counter")
+      $('output').removeClass("negative-counter") // Remove the class
     }
     
   });
