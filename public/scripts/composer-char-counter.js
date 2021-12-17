c719e15651b898e64fa2535a763cf82b9fd0f97f
Brenon <brenonparry@gmail.com>
2021-12-17 03:02:43 +0000

    Clean out old console logs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
   $("textarea").on("input", function() {
-    // console.log($(this).val().length)
+    
     let tweet = ($(this).val().length);
     $('output').val(140 - tweet)
-    // let numOfCharacters = 140;
-    // let remainingCharacters = numOfCharacters - tweet;
-
-    // console.log(remainingCharacters)
+    
     if ($('output').val() < 0) {
       $('output').addClass("negative-counter")
     } else {
